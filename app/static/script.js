ce449978415a2d7fb0a16a9fcbdfe97b86b263e0
shadowslayerhero <shadowslayerhero@gmail.com>
2022-05-17 21:27:02 +0800

    feat: add clear movie alert

--- app/static/script.js
+++ app/static/script.js
@@ -12,10 +12,20 @@ function show_movies() {
 }
 
 $(function () {
-	$("a#test").on("click", function (e) {
+	$("#clear-butt").click(function (e) {
 		e.preventDefault();
 		$.getJSON("/clear_movies", function (data) {
 			console.log(data);
 		});
+
+		$(".reset-alert").append(
+			`
+			<div class="alert alert-info alert-dismissible fade show" role="alert"
+			style="margin-top: 10px;">
+				<strong> Successfully Reset Movie Reviews </strong>
+				<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+			</div>
+			`
+		);
 	});
 });
